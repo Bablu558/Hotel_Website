2e42696cccb1a89d394de791f81689f8bac64a1c
Git-Github <kumarbablu03240@gmail.com>
2025-03-30 12:27:06 +0530

    h

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use((req,res,next)=>{
 // });
 
 
-app.use("/",listingRouter);
+app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
